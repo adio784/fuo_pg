b0a5f7cb6f85d0ad3a51aaa38a91ac3c9a46be0d
Ridwan ADIO <adioridwan784@gmail.com>
2023-09-18 16:15:16 +0100

    First commit to the git repo - FUO pg application portal

---
+++ email_verify.php
@@ -0,0 +1,52 @@
+<?php
+    // include head start here
+    require_once 'includes/head.php';
+?>
+<!-- Start wrapper-->
+ <div id="wrapper">
+  <!-- <div class="content-wrapper"> -->
+      <!-- Start Row -->
+      <div class="row">
+        <!-- Email verification section start here -->
+        <div class="col-lg-5 mt-5 mx-auto">
+          <br><br><br><br><br>
+          <div class="card">
+            <div class="card-body text-center">
+       <h5 class="card-title text-primary">Email Verification</h5>
+              <p class="card-text">An email verification has just been sent to your registered email address. Click on the lin from your email and copy the application ID to a safe place <br><br>Thanks!</p>
+        <hr>
+        <a href="index.php" class="btn btn-primary shadow-info waves-effect waves-light"><i class="fa fa-star mr-1"></i>Back to Sign In</a>
+            </div>
+          </div>
+        </div>
+        <!-- Email verification section end here -->
+      </div>
+      <!--End Row-->
+    <!-- End container-fluid-->
+    
+   </div><!--End content-wrapper-->
+   <!--Start Back To Top Button-->
+    <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
+    <!--End Back To Top Button-->
+  </div>
+  <!--End wrapper-->
+
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+	
+  <!-- simplebar js -->
+   <script src="assets/plugins/simplebar/js/simplebar.js"></script>
+  <!-- waves effect js -->
+  <script src="assets/js/waves.js"></script>
+  <!-- sidebar-menu js -->
+  <script src="assets/js/sidebar-menu.js"></script>
+  <!-- Custom scripts -->
+  <script src="assets/js/app-script.js"></script>
+	
+</body>
+
+<!-- white-version/cards-simple-cards.html  Wed, 31 Oct 2018 03:46:15 GMT -->
+</html>
