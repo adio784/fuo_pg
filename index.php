c57e7d70893e25014cc4a5314919e490dcccac9c
Ridwan ADIO <adioridwan784@gmail.com>
2023-09-19 12:00:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>first file in PG Portal </title>
-</head>
-<body>
-    <h1> Our Team Member </h1>
-</body>
-</html>
+
